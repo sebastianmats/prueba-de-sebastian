18b61b4a482a7fc1599a12557fb448293e2312a9
sebastian <>
2024-04-24 20:52:27 -0500

    en este ejercicio hice 1 y 2 problemas

---
+++ pruebaSebastian/src/main/java/com/mycompany/pruebasebastian/PruebaSebastian.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebasebastian;
+
+/**
+ *
+ * @author Doc Niki
+ */
+public class PruebaSebastian {
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la prueba de crear repositorio!");
+    }
+}
